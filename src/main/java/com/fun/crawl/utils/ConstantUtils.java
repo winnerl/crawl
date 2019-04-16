6b4fa493760ec89319d862b0b15a64a4835a2148
fangliang.wang <wfangliang@outlook.com>
2019-04-16 23:04:30 +0800

    LLLLL

---
+++ src/main/java/com/fun/crawl/utils/ConstantUtils.java
@@ -0,0 +1,8 @@
+package com.fun.crawl.utils;
+
+public class ConstantUtils {
+
+    public static final String LOGIN_JS_TEXT = "function getGid() {return \"xxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx\".replace(/[xy]/g, function (e) {var t = 16 * Math.random() | 0, n = \"x\" == e ? t : 3 & t | 8;return n.toString(16)}).toUpperCase()}function  getCallback() {return \"bd__cbs__\" + Math.floor(2147483648 * Math.random()).toString(36)}\n"; //cookie中sessionid
+
+
+}
--- src/main/java/com/fun/crawl/utils/PanCoreUtil.java
+++ src/main/java/com/fun/crawl/utils/PanCoreUtil.java
@@ -5,8 +5,14 @@ import okhttp3.*;
 import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.message.BasicNameValuePair;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
@@ -14,13 +20,19 @@ public class PanCoreUtil {
 
     private static final String CHARSET_NAME = "UTF-8";
 
+    final String UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36";
+    String Host = "";
+    String Accept = "";
+    String Referer = "";
+    String AcceptEncoding = "";
+    String AcceptLanguage = "";
+    String Cookie = "";
 
     private static final OkHttpClient mOkHttpClient = new OkHttpClient().newBuilder()
             .connectTimeout(10, TimeUnit.SECONDS)
             .readTimeout(30, TimeUnit.SECONDS)
             .writeTimeout(10, TimeUnit.SECONDS)
             .pingInterval(30, TimeUnit.SECONDS).build();
-
 
     /**
      * 该不会开启异步线程。
@@ -83,5 +95,109 @@ public class PanCoreUtil {
         return url + "?" + formatParams(params);
     }
 
+    /**
+     * 接口请求返回response
+     *
+     * @param host      请求接口的域名地址
+     * @param apiUrl    请求接口名称，格式如："/api/list"
+     * @param inputMap  参数集合Map<key,value>，key参数名称，value参数值,都是字符串
+     * @param method    提交方式：DELETE，POST，PUT，GET,"GET"直接地址请求，默认："POST"
+     * @param onlyValue 请求参数只有一个的时候的值(GET请求用到，其他请求为null即可)
+     * @return 响应结果
+     */
+    public static Response getRequest(String host) {
+        Request request = null;
+
+        return null;
+    }
+
+    /**
+     * 获取百度登陆的GID
+     *
+     * @param url
+     * @param params
+     * @return
+     */
+    public static String getGId() {
+        ScriptEngineManager mgr = new ScriptEngineManager();
+        ScriptEngine engine = mgr.getEngineByName("javascript");
+        try {
+            engine.eval(ConstantUtils.LOGIN_JS_TEXT);
+            Invocable inv = (Invocable) engine;
+            String res = (String) inv.invokeFunction("getGid", new String[]{"5050412", "D"});
+            return res;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 获取百度Callback
+     *
+     * @param url
+     * @param params
+     * @return
+     */
+    public static String getCallback() {
+        ScriptEngineManager mgr = new ScriptEngineManager();
+        ScriptEngine engine = mgr.getEngineByName("javascript");
+        try {
+            engine.eval(ConstantUtils.LOGIN_JS_TEXT);
+            Invocable inv = (Invocable) engine;
+            String res = (String) inv.invokeFunction("getCallback", new String[]{"5050412", "D"});
+            return res;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 获取百度Callback
+     *
+     * @param url
+     * @param params
+     * @return
+     */
+    public static String getToken(String gid, String callback, Map headers) {
+        ScriptEngineManager mgr = new ScriptEngineManager();
+        ScriptEngine engine = mgr.getEngineByName("javascript");
+        try {
+            engine.eval(ConstantUtils.LOGIN_JS_TEXT);
+            Invocable inv = (Invocable) engine;
+            String res = (String) inv.invokeFunction("getCallback", new String[]{"5050412", "D"});
+            return res;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 公共头部Header
+     *
+     * @return
+     */
+    public static Map getMainHeader() {
+        Map header = new HashMap();
+        header.put("Host", "https://pan.baidu.com");
+        header.put("User-Agent", "Mozilla/8.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36");
+        header.put("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+        header.put("Accept-Language", "zh-CN,zh;q=0.9");
+        header.put("upgrade-insecure-requests", "1");
+        header.put("Accept-Encoding", "gzip, deflate, br");
+        header.put("Connection", "keep-alive");
+        return header;
+    }
+
+    public static Response visitPost(String host, String apiUrl) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(getGId());
+//        System.out.println(getCallback());
+    }
 
 }
